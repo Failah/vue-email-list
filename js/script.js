ae273306bbed24697d94f64e154ba255ed01aa02
ValerioScharizza <valerio.schiarizza@live.it>
2022-09-20 13:58:52 +0200

    Created the script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 console.log('JS OK!');
 
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+
 const app = new Vue(
     {
         el: '#root',
-        data: 'eraseMe',
+        data: {
+            emails: [],
+        },
+
+        mounted() {
+            for (let i = 1; i <= 10; i++) {
+                axios
+                    .get(
+                        'https://flynn.boolean.careers/exercises/api/random/mail'
+                    )
+                    .then((response) => {
+                        const result = response.data;
+                        console.log(result.response);
+                        this.emails.push(result.response);
+                    });
+            }
+        }
     }
 )
